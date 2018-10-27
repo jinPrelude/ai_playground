af6b8e9eda396734f40c41d285c97e67a0986147
Uijin Jung <jin.Prelude@gmail.com>
2018-10-27 09:49:13 +0900

    init

--- eager.py
+++ eager.py
@@ -1,38 +1,31 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.contrib.eager.python import tfe
-from tensorflow.python.keras.datasets import mnist
+from tensorflow.examples.tutorials.mnist import input_data
 
-tf.enable_eager_execution()
-tf.set_random_seed(0)
-np.random.seed(0)
+img_size = 28
+batch_size = 128
+epochs = 8
 
-class Block(tf.keras.Model) :
-    def __init__(self, filter, kernel, stride):
-        super(Block, self).__init__()
-        self.conv = tf.keras.layers.Conv2D(filters=filter, kernel_size=(kernel,kernel), strides=(stride, stride),
-                                           padding='same', kernel_initializer='he_normal')
-        self.bn = tf.keras.layers.BatchNormalization()
-        self.relu = tf.nn.relu
+class generator():
+    def __init__(self, sess, lr):
+        self.sess = sess
+        self.lr = lr
 
-    def call(self, inputs, training=None, mask=None):
-        x = self.conv(inputs)
-        x = self.bn(x)
-        x = self.relu(x)
-        return x
+    def create_generator(self):
+        inputs = tf.placeholder(name='generator_input', shape=[None, 100, 1], dtype=tf.float32)
+        w1 = tf.layers.dense(inputs, [None, 4*4*1024], activation=tf.nn.relu)
+        w1_rs = tf.reshape(w1, [None, 4, 4, 1024])
+        w2 = tf.nn.conv2d_transpose(w1_rs, output_shape=[None, 9, 9, 512], strides=[2, 2])
 
-class CNN(tf.keras.Model) :
-    def __init__(self, num_class):
-        super(CNN, self).__init__()
-        self.block = Block(16, 3, 2)
-        self.dense = tf.keras.layers.Dense(num_class)
 
-    def call(self, inputs, training=None, mask=None):
-        x = self.block(inputs)
-        output = self.dense(x)
-        return output
+        """
+        conv1 = tf.layers.conv2d(inputs=inputs, filters=16, kernel_size=[3, 3], padding='same',
+                              activation=tf.nn.relu, kernel_initializer='he_normal')
 
-device = '/cpu:0' if tfe.num_gpus() == 0 else '/gpu:0'
+        conv2 = tf.layers.conv2d(inputs=conv1, filters=32, kernel_size=[3, 3], padding='same',
+                                 activation=tf.nn.relu, kernel_initializer='he_normal')
 
-with tf.device(device) :
-    
+        conv_one = tf.layers.conv2d(inputs=conv1, filters=1, kernel_size=[1, 1], strides=[1,1],
+                                    activation=tf.nn.relu, kernel_initializer='he_normal')
+        """
+
